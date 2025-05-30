a8a3144ac89d8f0e4d8d2750c72be0e6b81ffaba
MateoPerez2 <mateopaparella@hotmail.com>
2025-05-30 18:40:36 -0300

    locals y redirects faltan validaciones

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -7,7 +7,15 @@ const bcryptjs = require("bcryptjs");
 const usersController = {
 
     register: function(req, res) {
-        res.render('register')
+
+
+        if (req.session.user != undefined) {
+            return res.redirect("/users/profile")
+        } else {
+            res.render('register')
+        }
+
+        
     },
     create: function(req, res) {
        // recuperar los datos del form
@@ -38,7 +46,11 @@ const usersController = {
        
    },
     login: function(req, res) {
-        res.render('login')
+        if (req.session.user != undefined) {
+            return res.redirect("/users/profile")
+        } else {
+            res.render('login')
+        }
     },
     createLogin: function(req, res) {
         // recuperar los datos del form
@@ -49,7 +61,13 @@ const usersController = {
         }
        
         // validar que el mail y la pasword sean correctas
-
+        let user = db.User.findOne({
+            where: {
+                email: email
+            }
+        }) //las validaciones no estan hechas
+        
+        
 
         //poner en session
         req.session.user = userInfo;
