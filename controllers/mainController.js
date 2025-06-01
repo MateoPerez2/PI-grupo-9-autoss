f3bdea31e6dea8581808f637064a9954040e8394
GastiK28 <gkozak@udesa.edu.ar>
2025-06-01 20:51:29 -0300

    arreglando un par de cosas

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,28 +1,30 @@
-const { Association } = require("sequelize");
+const { Association, where } = require("sequelize");
 const autos = require("../db") //La requerimos para tulizar su lista de prodsuctos
 const db = require("../db/models");
+const op = db.Sequelize.Op;
 const Product = db.Product;
 const mainController = { //Creo un objeto literal con metodos index y search (metodo=funcion dentro de un objeto literal). Cada uno hace referencia a una vista y a un sufijo
 
-    index: function(req, res) {
+    index: function (req, res) {
 
-        Product.findAll({include:[{association:'user'}]})
+        Product.findAll({ include: [{ association: 'user' }] })
             .then(function (resultados) {
-                console.log(resultados);
-                return res.render('index', {producto: resultados })
+                return res.render('index', { producto: resultados })
             })
             .catch(function (err) {
                 return res.send(err);
             })
 
     },
-    search: function(req, res) {
-        //const search = req.params.search
-        res.render('search-results', { producto: autos.productos})
+    search: function (req, res) {
+        res.render('search-results', {producto: autos.productos} )
+
+
     }
-    
-    
-    
+
+
+
+
 
 
 }
