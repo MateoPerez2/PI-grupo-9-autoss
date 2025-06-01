517c5221b5de70a12e53345babecc18c81d1a66a
GastiK28 <gkozak@udesa.edu.ar>
2025-06-01 11:37:56 -0300

    product ejs conecntando a la base de datos y haciendo hipervinculos con los perfiles

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,11 +1,22 @@
 const autos = require('../db')
+const db = require("../db/models");
+const Product = db.Product;
 
 const productController = {
 
     
     product: function(req, res) {
         let id = req.params.id; // obetenes al parametro que ingreso el usuario dentro de la ruta,  osea el producto que yo toque con la imagen, osea el id del producto.
-        res.render('product', { filtroId: autos.filtrarPorId(id)}) // mostramos la vista de product. y como segundo parametro recibe el objeto literario que tienen la propiedad filtrar id cuya propiedad filitrar id va a tener porducto seleccionado
+        
+        Product.findByPk(id,{include: [{association:'comments', include: [{association:'user'}]}]})
+
+         .then(function (resultados) {
+                return res.render('product', { producto: resultados })
+            })
+            .catch(function (err) {
+                return res.send(err);
+            })
+        
     },
     productAdd: function(req, res) {
         res.render('product-add',  {profile: autos})
